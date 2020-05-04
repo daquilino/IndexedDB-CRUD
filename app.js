20b331905bf82556863a7ca31d6743a97b2b2837
D <douglasjaquilino@gmail.com>
2020-05-04 16:20:09 -0400

    submit event listener for form

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 console.log("app.js loaded!")
+
+
+const addUserForm = document.querySelector("#addUserForm");
+const userIdInput = document.querySelector("#userId");
+
+addUserForm.addEventListener("submit", e => {
+    e.preventDefault();
+    console.log("user form submitted")
+
+    console.log(e.target.elements[0])
+ 
+})
