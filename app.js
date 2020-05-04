1bbd1d4e6029a1f18017e724750de5bd5a5d3988
D <douglasjaquilino@gmail.com>
2020-05-04 16:39:25 -0400

    when form is sumbitted user is inserted  into db

--- app.js
+++ app.js
@@ -3,11 +3,28 @@ console.log("app.js loaded!")
 
 const addUserForm = document.querySelector("#addUserForm");
 const userIdInput = document.querySelector("#userId");
+const fnameInput = document.querySelector("#fname");
+const lnameInput = document.querySelector("#lname");
+const stateInput = document.querySelector("#state");
 
 addUserForm.addEventListener("submit", e => {
     e.preventDefault();
     console.log("user form submitted")
 
-    console.log(e.target.elements[0])
+    let userID = userIdInput.value.trim()
+    let firstName = fnameInput.value.trim()
+    let lastName = lnameInput.value.trim()
+    let state = stateInput.value.trim()
+
+
+    let newUser = {
+        userID,
+        firstName,
+        lastName,
+        state
+    }
+
+
+    createUser(newUser);
  
 })
