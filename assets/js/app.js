e889c6638199f21597ea930e37c6f2a9ae483bc8
D <douglasjaquilino@gmail.com>
2020-05-10 20:53:30 -0400

    consts for inputs selectors

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,29 +2,32 @@ console.log("app.js loaded!")
 
 
 const addContactForm = document.querySelector("#addContact");
-const userIdInput = document.querySelector("#userId");
-const fnameInput = document.querySelector("#fname");
-const lnameInput = document.querySelector("#lname");
-const stateInput = document.querySelector("#state");
+
+const fnameInput = document.querySelector("#fnameInput");
+const lnameInput = document.querySelector("#lnameInput");
+const emailInput = document.querySelector("#emailInput");
+const telInput = document.querySelector("#telInput");
+
 
 addContactForm.addEventListener("submit", e => {
     e.preventDefault();
-    console.log("user form submitted")
+    // console.log("user form submitted")
 
-    let userID = userIdInput.value.trim()
-    let firstName = fnameInput.value.trim()
-    let lastName = lnameInput.value.trim()
-    let state = stateInput.value.trim()
-
-
-    let newUser = {
-        userID,
-        firstName,
-        lastName,
-        state
-    }
+    // let userID = userIdInput.value.trim()
+    // let firstName = fnameInput.value.trim()
+    // let lastName = lnameInput.value.trim()
+    // let state = stateInput.value.trim()
 
 
-    createUser(newUser);
+    // let newUser = {
+    //     userID,
+    //     firstName,
+    //     lastName,
+    //     state
+    // }
+
+
+    // createUser(newUser);
  
 })
+
